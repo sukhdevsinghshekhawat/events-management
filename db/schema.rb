a51a5f9f23dff80f985b70702d3d8ced850d296e
SukhdevSinghShekhawat <sukhdevsingh00133@gmail.com>
2025-11-02 12:41:59 +0530

    Upgrade to Rails 7.1 for Render deploy fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_09_22_114054) do
+ActiveRecord::Schema[7.1].define(version: 2025_11_02_070714) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
